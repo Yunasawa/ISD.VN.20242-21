a358580d436eec68cb16f75eddd70bc9c937891e
Yunasawa <yunasawa100@gmail.com>
2025-04-29 15:24:39 +0700

    [TES] Test Account Validation

---
+++ Scripts/Tests/ProgramTest.cs
@@ -0,0 +1,28 @@
+﻿using HotelReservation;
+
+namespace MediaStore.Tests
+{
+    public class ProgramTest
+    {
+        public static void Main(string[] args)
+        {
+            RegisterEvent();
+
+            var DataContainer = new DataContainer();
+
+            DataContainer.Accounts = CsvSerializer.LoadFromCsv(@"C:\Users\Yunasawa\Documents\Projects\ISD.VN.20242-21\Samples\AccountData.csv");
+            Function.AccountValidater.SignAccount(AccountSignType.SignUp, AccountVerificationType.PhoneNumber, "6750443676", "mwU6cwgS9zeN", "mwU6cwgS9zeN");
+            Function.AccountValidater.SignAccount(AccountSignType.SignUp, AccountVerificationType.PhoneNumber, "6754483676", "mwU6cwgS9zeN", "mwU6cwgS9zeN");
+            Function.AccountValidater.SignAccount(AccountSignType.SignIn, AccountVerificationType.PhoneNumber, "6750443676", "mwU6cwgS9zeN", "mwU6cwgS9zeN");
+            Function.AccountValidater.SignAccount(AccountSignType.SignIn, AccountVerificationType.PhoneNumber, "6754483676", "mwU6cwgS9zeN", "mwU6cwgS9zeN");
+        }
+
+        private static void RegisterEvent()
+        {
+            Event.OnAccountVerificated += (result) =>
+            {
+                Console.WriteLine($"Account verification result: {result}");
+            };
+        }
+    }
+}
