c32c442193b7ba731396ae36a955f6f0dbff8022
Yunasawa <yunasawa100@gmail.com>
2025-05-14 15:05:03 +0700

    [WIP]

---
+++ Assets/Mains/Scripts/Handlers/Functions/Function.HotelFilterer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace YNL.Checkotel
+{
+    public partial class Function
+    {
+        public class HotelFilterer
+        {
+            public static List<UID> Filter()
+            {
+                return null;
+            }
+        }
+    }
+}
