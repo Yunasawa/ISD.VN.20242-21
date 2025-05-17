214f55e97efcf5fa1fef6f80a6bc68ec09ef1d1a
NHP234 <Phuc.NH225905@sis.hust.edu.vn>
2025-05-17 11:41:41 +0700

    [WIP] Add DataContainer

---
+++ Assets/Mains/Scripts/Handlers/Managers/DataContainer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace YNL.Checkotel
+{
+    public class DataContainer
+    {
+        public UID AccountID = new();
+        public static List<Account> Accounts = new();
+    }
+}
