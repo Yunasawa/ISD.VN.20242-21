c8ec9ad4822d6d8401e628ab18ec98510e998881
Thanhlong04 <halong0416@gmail.com>
2025-05-27 15:11:36 +0700

    [FEA] Search Views

--- Assets/Mains/Scripts/Views/Pages/Search Views/SearchViewMainPageUI.cs
+++ Assets/Mains/Scripts/Views/Pages/Search Views/SearchViewMainPageUI.cs
@@ -1,11 +1,19 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine.UIElements;
+using YNL.Utilities.Addons;
+using YNL.Utilities.Extensions;
 using YNL.Utilities.UIToolkits;
 
 namespace YNL.JAMOS
 {
+    public enum SearchingSuggestionType : byte { Creator, Book, CD, DVD, LP }
+
     public partial class SearchViewMainPageUI : ViewPageUI
     {
+        private SerializableDictionary<UID, Product.Data> _products => Main.Database.Products;
+
         private VisualElement _closeButton;
         private TextField _searchInput;
         private VisualElement _productTypeField;
@@ -14,6 +22,9 @@ namespace YNL.JAMOS
         private Label _suggestionLabel;
         private ListView _suggestionList;
         private Label _emptyText;
+        private SerializableDictionary<Product.Type, ProductTypeButtonUI> _productTypeButtons = new();
+
+        private List<(SearchingSuggestionType type, string value)> _suggestionValues = new();
 
         protected override void VirtualAwake()
         {
@@ -31,7 +42,6 @@ namespace YNL.JAMOS
             _searchInput = Root.Q("SearchField").Q("SearchInput") as TextField;
             _searchInput.RegisterValueChangedCallback(OnValueChanged_SearchInput);
             _searchInput.RegisterCallback<FocusInEvent>(OnFocusIn_SearchInput);
-            _searchInput.RegisterCallback<FocusOutEvent>(OnFocusOut_SearchInput);
 
             _searchButton = Root.Q("SearchButton");
             _searchButton.RegisterCallback<PointerUpEvent>(OnClicked_SearchButton);
@@ -46,14 +56,37 @@ namespace YNL.JAMOS
             _suggestionView.Remove(suggestionScroll);
 
             _suggestionList = _suggestionView.Q("SuggestionList") as ListView;
+            _suggestionList.fixedItemHeight = 100;
+            _suggestionList.itemsSource = _suggestionValues;
+            _suggestionList.makeItem = () => new SearchingSuggestionItemUI();
+            _suggestionList.bindItem = (element, index) =>
+            {
+                var item = element as SearchingSuggestionItemUI;
+                item.OnSelected = ApplySearchingInput;
+                if (index < _suggestionValues.Count)
+                {
+                    item.Apply(_suggestionValues[index].type, _suggestionValues[index].value);
+                }
+            };
 
             _emptyText = _suggestionView.Q("EmptyResultLabel") as Label;
+            
+            var typeField = _productTypeField.Q("TypeField");
+            foreach (Product.Type type in Enum.GetValues(typeof(Product.Type)))
+            {
+                var productTypeButton = new ProductTypeButtonUI(typeField, type);
+                _productTypeButtons.Add(type, productTypeButton);
+            }
+
+            ProductTypeButtonUI.OnSelected += OnProductTypeSelected;
         }
 
         protected override void Initialize()
         {
-            _emptyText.SetDisplay(DisplayStyle.None);
+            _suggestionList.SetDisplay(DisplayStyle.None);
             _suggestionView.SetDisplay(DisplayStyle.None);
+
+            _productTypeButtons[Product.Type.None].OnSelected_Button();
         }
 
         private void OnClicked_CloseButton(PointerUpEvent evt)
@@ -68,7 +101,47 @@ namespace YNL.JAMOS
 
         private void OnValueChanged_SearchInput(ChangeEvent<string> evt)
         {
+            var value = evt.newValue;
+            _suggestionValues.Clear();
 
+            if (value == string.Empty)
+            {
+                _suggestionLabel.SetText("Searching history");
+                _suggestionValues = Main.Runtime.Data.SearchingHistory;
+                _emptyText.SetText("You haven't searched for anything yet!");
+            }
+            else
+            {
+                _suggestionLabel.SetText("Suggestions");
+                _emptyText.SetText("No suggestion matching your search!");
+
+                foreach (var pair in _products)
+                {
+                    var product = pair.Value;
+
+                    if (product.Title.FuzzyContains(value))
+                    {
+                        _suggestionValues.Add((product.Type.ToProductType(), product.Title));
+                    }
+                    foreach (var creator in product.Creators)
+                    {
+                        if (creator.FuzzyContains(value))
+                        {
+                            _suggestionValues.Add((SearchingSuggestionType.Creator, creator));
+                        }
+                    }
+                }
+            }
+
+            bool emptyResult = _suggestionValues.IsEmpty();
+
+            _emptyText.SetDisplay(emptyResult ? DisplayStyle.Flex : DisplayStyle.None);
+            _suggestionList.SetDisplay(emptyResult ? DisplayStyle.None : DisplayStyle.Flex);
+
+            if (!emptyResult)
+            {
+                _suggestionList.RebuildListView(_suggestionValues);
+            }
         }
 
         private void OnFocusIn_SearchInput(FocusInEvent evt)
@@ -79,10 +152,19 @@ namespace YNL.JAMOS
             _suggestionView.SetDisplay(DisplayStyle.Flex);
         }
 
-        private void OnFocusOut_SearchInput(FocusOutEvent evt)
+        private void ApplySearchingInput(string input)
         {
+            _searchInput.SetValueWithoutNotify(input);
+
             _productTypeField.SetDisplay(DisplayStyle.Flex);
             _searchButton.SetDisplay(DisplayStyle.Flex);
+
+            Main.Runtime.SearchingInput = input;
+        }
+
+        private void OnProductTypeSelected(Product.Type type)
+        {
+            Main.Runtime.SearchingProductType = type;
         }
     }
 }
--- Assets/Mains/Scripts/Views/Pages/Search Views/SearchViewResultPageUI.cs
+++ Assets/Mains/Scripts/Views/Pages/Search Views/SearchViewResultPageUI.cs
@@ -1,17 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using UnityEngine;
 using UnityEngine.UIElements;
-using YNL.Utilities.Addons;
-using YNL.Utilities.Extensions;
 using YNL.Utilities.UIToolkits;
 
 namespace YNL.JAMOS
 {
     public class SearchViewResultPageUI : ViewPageUI
     {
+        private Label _searchText;
+        private VisualElement _typeIcon;
+        private ListView _resultList;
+        private Label _emptyText;
+
         protected override void VirtualAwake()
         {
 
@@ -24,11 +22,54 @@ namespace YNL.JAMOS
 
         protected override void Collect()
         {
-            
+            var resultPage = Root.Q("SearchingResultPage");
+
+            var searchBar = resultPage.Q("SearchBar");
+            searchBar.RegisterCallback<PointerUpEvent>(OnClicked_SearchBar);
+
+            _searchText = searchBar.Q("SearchField").Q("SearchText") as Label;
+
+            _typeIcon = searchBar.Q("SearchField").Q("TypeIcon");
+
+            var toolBar = resultPage.Q("ToolBar");
+
+            var sortButton = toolBar.Q("SortingButton");
+            sortButton.RegisterCallback<PointerUpEvent>(OnClicked_SortButton);
+
+            var filterButton = toolBar.Q("FilteringButton");
+            filterButton.RegisterCallback<PointerUpEvent>(OnClicked_FilterButton);
+
+            var resultScroll = resultPage.Q("ResultScroll");
+            resultPage.Remove(resultScroll);
+
+            _emptyText = resultPage.Q("EmptyText") as Label;
+
+            _resultList = resultPage.Q("ResultList") as ListView;
+            _resultList.Q("unity-content-container").SetFlexGrow(1);
+            _resultList.Q<ScrollView>().verticalScrollerVisibility = ScrollerVisibility.Hidden;
         }
 
         protected override void Refresh()
         {
+            _typeIcon.SetBackgroundImage(Main.Resources.Icons[Main.Runtime.SearchingProductType.ToString()]);
+
+            var searchInput = string.IsNullOrEmpty(Main.Runtime.SearchingInput) ? "Anything" : Main.Runtime.SearchingInput;
+            _searchText.SetText(searchInput);
+        }
+
+        private void OnClicked_SearchBar(PointerUpEvent evt)
+        {
+            Marker.OnViewPageSwitched?.Invoke(ViewType.SearchViewMainPage, true, false);
+        }
+
+        private void OnClicked_SortButton(PointerUpEvent evt)
+        {
+
+        }
+
+        private void OnClicked_FilterButton(PointerUpEvent evt)
+        {
+
         }
     }
-}
+}   
